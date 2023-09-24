151476b99e27489a4baa7f0e5527da03da9fc6a4
Anatoly <kolganovpix@yandex.ru>
2023-09-25 01:10:40 +0500

    Добавит верстку page 404

--- src/pages/404/page-404.tsx
+++ src/pages/404/page-404.tsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../config';
+
 function Page404(): JSX.Element {
-  return <h1>Not a found</h1>;
+  return (
+    <main className="page-content">
+      <div className="container">
+        <div className="page-content__title-wrapper">
+          <h1 className="subtitle page-content__subtitle">
+            квесты в Санкт-Петербурге
+          </h1>
+          <h2>Страница не найдена</h2>
+        </div>
+        <Link to={AppRoute.Root} className="link">
+          Вернуться на главную
+        </Link>
+      </div>
+    </main>
+  );
 }
 
 export default Page404;
